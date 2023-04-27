52468bdfb9f587c958b51d875902a5309ffe94e1
RustamKhudoyarov <khrust_lord@mail.ru>
2023-04-27 09:49:47 -0500

    -- refactoring

---
+++ src/test/java/steps/StepsDef.java
@@ -0,0 +1,31 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.example.Warrior;
+import org.junit.Assert;
+
+public class StepsDef {
+    private final Warrior warrior = new Warrior("Test",100,10);
+
+    @Given(": The warrior have {int} health")
+    public void the_warrior_have_health(Integer health) {
+
+        warrior.heal(health);
+        Assert.assertTrue(warrior.heal(health) > 0);
+    }
+
+    @When(": the warrior got {int} points damage")
+    public void the_warrior_got_points_damage(Integer damage) {
+        int remainHealth = warrior.takeDamage(damage);
+        Assert.assertTrue( remainHealth > 0);
+
+    }
+
+    @Then(": health warrior was decrease on {int} points health")
+    public void health_warrior_was_decrease_on_points_health(Integer damage) {
+        warrior.takeDamage(damage);
+        Assert.assertTrue( warrior.takeDamage(damage) < 100);
+    }
+}
---
+++ src/test/resources/features/Feature.feature
@@ -0,0 +1,5 @@
+Feature: The warrior had a damage 99
+  Scenario: THe warrior has 100 health and get 99 points damage
+    Given : The warrior have 100 health
+    When : the warrior got 99 points damage
+    Then : health warrior was decrease on 99 points health
